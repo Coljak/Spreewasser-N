4bbd7ee44def46464aae52fe29a84d53f6f9e069
ColjaK <colja.krugmann@zalf.de>
2026-05-15 15:33:11 +0200

    entrypoint

--- app/klim4cast/static/klim4cast/js/map_thredds_klim4cast.js
+++ app/klim4cast/static/klim4cast/js/map_thredds_klim4cast.js
@@ -1,12 +1,4 @@
-/*
-This script belongs to the timelapse page. It is using leaflet.timeDimension
-https://github.com/socib/Leaflet.TimeDimension/blob/master/README.md#lcontroltimedimension
-and the bootstrap datepicker https://bootstrap-datepicker.readthedocs.io/en/latest/
 
-This javaScript is the derived from map_thredds....js in the django app swn
-*/
-
-import { getOrCreateLegendList, htmlLegendPlugin } from '/static/vendor/chartjs/chartjs-html-legend.js';
 const palettes = [ "default", "default-inv", "div-BrBG", "div-BrBG-inv", "div-BuRd", "div-BuRd-inv", "div-BuRd2", "div-BuRd2-inv", "div-PRGn", "div-PRGn-inv", "div-PiYG", "div-PiYG-inv", "div-PuOr", "div-PuOr-inv", "div-RdBu", "div-RdBu-inv", "div-RdGy", "div-RdGy-inv", "div-RdYlBu", "div-RdYlBu-inv", "div-RdYlGn", "div-RdYlGn-inv", "div-Spectral", "div-Spectral-inv", "psu-inferno", "psu-inferno-inv", "psu-magma", "psu-magma-inv", "psu-plasma", "psu-plasma-inv", "psu-viridis", "psu-viridis-inv", "seq-BkBu", "seq-BkBu-inv", "seq-BkGn", "seq-BkGn-inv", "seq-BkRd", "seq-BkRd-inv", "seq-BkYl", "seq-BkYl-inv", "seq-BlueHeat", "seq-BlueHeat-inv", "seq-Blues", "seq-Blues-inv", "seq-BuGn", "seq-BuGn-inv", "seq-BuPu", "seq-BuPu-inv", "seq-BuYl", "seq-BuYl-inv", "seq-GnBu", "seq-GnBu-inv", "seq-Greens", "seq-Greens-inv", "seq-Greys", "seq-Greys-inv", "seq-GreysRev", "seq-GreysRev-inv", "seq-Heat", "seq-Heat-inv", "seq-OrRd", "seq-OrRd-inv", "seq-Oranges", "seq-Oranges-inv", "seq-PuBu", "seq-PuBu-inv", "seq-PuBuGn", "seq-PuBuGn-inv", "seq-PuRd", "seq-PuRd-inv", "seq-Purples", "seq-Purples-inv", "seq-RdPu", "seq-RdPu-inv", "seq-Reds", "seq-Reds-inv", "seq-YlGn", "seq-YlGn-inv", "seq-YlGnBu", "seq-YlGnBu-inv", "seq-YlOrBr", "seq-YlOrBr-inv", "seq-YlOrRd", "seq-YlOrRd-inv", "seq-cubeYF", "seq-cubeYF-inv", "x-Ncview", "x-Ncview-inv", "x-Occam", "x-Occam-inv", "x-Rainbow", "x-Rainbow-inv", "x-Sst", "x-Sst-inv",]
 
 // in this object, defaults are set for the color palette and the min and max values for the color scale. This can be moved to the thredds server metadata, but has to be modified theer for each dataset
@@ -206,77 +198,6 @@ function createBaseMap() {
 function showCoordinates (e) {
     alert(`Länge: ${e.latlng.lat}, Breite: ${e.latlng.lng}`);
 };
-
-function createChart(data) {
-    
-    let modalTile = document.getElementById('clim4castChartTitle');
-    modalTile.innerHTML = `${data.long_name} at ${data.latitude.toFixed(2)}, ${data.longitude.toFixed(2)}`;
-
-    // Ensure that the modal is shown before attempting to create the chart
-    let chartDiv = document.getElementById('clim4castChartDiv');
-    console.log(chartDiv);
-
-  
-
-    // Add a small delay before creating the chart to make sure the modal (and canvas) is fully visible
-    setTimeout(function() {
-        const chart = new Chart(chartDiv, {
-            type: "line",
-            data: {
-                labels: data.dates, // the date labels
-                datasets: [{
-                    label: `${data.long_name} ${data.units}`,
-                    data: data.values,
-                    borderColor: 'rgba(75, 192, 192, 1)', // Set color for the line
-                    fill: false // Set whether the area under the line should be filled
-                }]
-            },
-
-            plugins: [htmlLegendPlugin],
-            options: {
-                responsive: true,
-                plugins: {
-                    legend: {
-                        position: 'right' // Position of the legend
-                    },
-                    zoom: {
-                        zoom: {
-                        wheel: {
-                            enabled: true,
-                        },
-                        pinch: {
-                            enabled: true
-                        },
-                        mode: 'x',
-                        }
-                    },
-            
-                },
-                scales: {
-                    x: {
-                        type: 'category',
-                        labels: data.dates, // the dates as labels on the x-axis
-                        title: {
-                            display: true,
-                            text: 'Dates'
-                        }
-                    },
-                    y: {
-                        title: {
-                            display: true,
-                            text: `${data.units}`
-                        }
-                    }
-                }
-            },
-        });
-
-        chart.update();
-    }, 200);  // Delay of 200ms (adjust if necessary)
-
-};
-
-
 
 function showData (e) {
     let nc = datasetSelector.value;
--- scripts/entrypoint.prod.sh
+++ scripts/entrypoint.prod.sh
@@ -2,8 +2,6 @@
 
 set -e # Exit immediately if a command exits with a non-zero status.
 
-python manage.py collectstatic --noinput
-python manage.py migrate
 
 # Wait for the database to be ready
 echo "Waiting for the database..."
@@ -11,5 +9,7 @@ while ! python3 manage.py wait_for_db; do
   sleep 2
 done
 echo "Database is ready."
+python manage.py collectstatic --noinput
+python manage.py migrate
 
 gunicorn app.wsgi:application --bind 0.0.0.0:8000 --workers 3
